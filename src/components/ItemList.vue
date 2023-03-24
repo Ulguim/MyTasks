4b54cdb6efca8d017c28b9753314903f3ac325f2
william.ulguim <william.ulguim.ext@wundermanthompson.com>
2023-03-24 13:46:48 -0300

    :art: Create Feature to add new Tasks

---
+++ src/components/ItemList.vue
@@ -0,0 +1,128 @@
+<template>
+  <div id="#itemList">
+    <div class="listBox">
+      <div :key="item.id" v-for="item in lista" class="itemBox">
+        <text
+          style="
+            font-size: 16px;
+            color: black;
+            margin-inline: 20px;
+            margin-right: auto;
+            text-align: start;
+          "
+        >
+          {{ item.name }}</text
+        >
+        <button @click="deleteItem(item.id)" class="DeleteButton">
+          Deletar
+        </button>
+        <button class="button">Concluir</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "itemList",
+  data() {
+    return {};
+  },
+  props: ["lista", "delete"],
+  methods: {
+    deleteItem(key) {
+      this.delete(key);
+    },
+  },
+};
+</script>
+
+<style scoped>
+#itemList {
+  min-width: 280px;
+  width: 500px;
+  max-height: 400px;
+}
+.listBox {
+  padding-top: 20px;
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  gap: 20px;
+  min-width: 280px;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  max-height: 500px;
+  min-height: 500px;
+  background-color: #f5f5f5;
+  border-radius: 5px;
+
+  align-items: top;
+}
+
+.listBox::-webkit-scrollbar {
+  width: 5px;
+}
+
+.listBox::-webkit-scrollbar-track {
+  border-radius: 10px;
+}
+
+.listBox::-webkit-scrollbar-thumb {
+  background: #4caf50;
+  border-radius: 10px;
+}
+
+.listBox::-webkit-scrollbar-thumb:hover {
+  background: #4caf50;
+}
+.listData {
+  min-width: 50px;
+  min-height: 20px;
+  list-style-type: none;
+}
+
+.button {
+  background-color: #4caf50;
+  border: none;
+  color: white;
+  width: 150px;
+  border-radius: 5px;
+  text-align: center;
+  display: inline-block;
+  font-size: 16px;
+  margin-right: 20px;
+  margin-left: 10px;
+  height: 25px;
+  padding-left: 10px;
+}
+
+.DeleteButton {
+  background-color: red;
+  border: none;
+  color: white;
+  width: 150px;
+  border-radius: 5px;
+  text-align: center;
+  display: inline-block;
+  font-size: 16px;
+  height: 25px;
+  margin-right: 5px;
+}
+
+.itemBox {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  height: 50px;
+  min-height: 50px;
+  margin-left: 10px;
+  margin-right: 10px;
+  background-color: white;
+  border-radius: 5px;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+}
+</style>
---
+++ src/components/TaskComp.vue
@@ -0,0 +1,92 @@
+<template>
+  <div id="task">
+    <h3>Minhas Tarefas</h3>
+
+    <form @submit.prevent="addTask">
+      <div class="formFields">
+        <input
+          class="inputField"
+          v-model="task"
+          type="text"
+          placeholder="Exercício"
+        />
+        <button class="submitButton" type="submit">Adicionar</button>
+      </div>
+    </form>
+    <ItemList :delete="deleteTask" :lista="tasks"></ItemList>
+  </div>
+</template>
+''
+
+<script>
+import ItemList from "./ItemList.vue";
+
+export default {
+  name: "TaskComp",
+  components: { ItemList },
+  data() {
+    let data = [];
+    if (localStorage.getItem("MyTasks") != null) {
+      data = JSON.parse(localStorage.getItem("MyTasks"));
+    }
+    return {
+      task: "",
+      msg: "Bem Vindo: ",
+      tasks: data || [],
+    };
+  },
+  methods: {
+    addTask() {
+      this.task != ""
+        ? this.tasks.push({ name: this.task, id: Date.now() })
+        : alert("Digite uma tarefa");
+      this.task = "";
+      localStorage.setItem("MyTasks", JSON.stringify(this.tasks));
+    },
+    deleteTask(key) {
+      this.tasks = this.tasks.filter((item) => item.id != key);
+      localStorage.setItem("MyTasks", JSON.stringify(this.tasks));
+    },
+  },
+};
+</script>
+
+<style scoped>
+#task {
+  display: flex;
+  width: 100%;
+  margin-bottom: 10px;
+  width: 100%;
+  flex-direction: column;
+}
+
+.inputField {
+  width: 500px;
+  height: 50px;
+  max-width: 500px;
+  border: none;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+  color: gray.300;
+  border-radius: 5px;
+}
+
+.submitButton {
+  background-color: #4caf50;
+  border: none;
+  color: white;
+  width: 500px;
+  border-radius: 5px;
+  text-align: center;
+  display: inline-block;
+  font-size: 16px;
+  height: 50px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+.formFields {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+</style>
